16a4d6194d837a550414c1b2f7b758fa6c4d54af
franco.p <franco_urc@hotmail.com>
2022-02-09 17:50:53 +0100

    se elimino la copia de la clase Resta

--- TrabajoEnEquipo/src/git/Resta.java
+++ TrabajoEnEquipo/src/git/Resta.java
@@ -1,20 +1,18 @@
-package git;
-
 /**
- * Esta clase esta dedicada a implementar los mtodos de resta necesarios para implementar una series de clase que compongan una calculadora.
- * Cada clase que compone los metodos de resta estan documentados para poder instruirse en caso de necesitar informaci�n del m�todo. 
+ * Esta clase esta dedicada a implementar los metodos de resta necesarios para implementar una series de clase que compongan una calculadora.
+ * Cada clase que compone los metodos de resta estan documentados para poder instruirse en caso de necesitar informacionn del metodo. 
  * @author Franco Pellizzari
  * @version version 1.0
  *
  */
 
-public class RestaGit {
+public class Resta {
 
 	/** Metodo que calcula la diferencia entre dos numeros reales.
 	 * La diferencia se obtiene restando los numeros en el orden que se hayan introducido.
 	 * 
 	 * @param dos numeros reales de los cuales se desea obtener la diferencia.
-	 * @return un numero que seria el resultado de la resta.
+	 * @return un numero que sera el resultado de la resta.
 	 */
 	  
 	public double resta1(double numero) {
@@ -26,9 +24,9 @@ public class RestaGit {
 	/**
 	 * Metodo que calcula la diferencia entre dos numeros enteros..
 	 * La diferencia se obtiene restando los numeros en el orden que se hayan introducido.
-	 * En caso de que se ingresen numeros con decimales, la clase daria error. 
+	 * En caso de que se ingresen numeros con decimales, la clase dara error. 
 	 * @param dos numeros enteros de los cuales se desea obtener la diferencia.
-	 * @return devolveria un numero entero que seria el resultado de la resta.
+	 * @return devolvera un numero entero que sera el resultado de la resta.
 	 */
 	public int resta2(int numero) {
 		int resultado = 0;
@@ -37,10 +35,10 @@ public class RestaGit {
 	    }
 	
 	/** Metodo que calcula la diferencia entre 3 numeros reales.
-	 * La diferencia se obtiene restando los numeros en el orden que se hayan introducido.
+	 * La diferencia se obtiene restando los n�meros en el orden que se hayan introducido.
 	 * 
 	 * @param tres numeros reales de los cuales se desea obtener la diferencia.
-	 * @return un numero que seria el resultado de la resta.
+	 * @return un numero que sera el resultado de la resta.
 	 */
 	  
 	public double restaVersion1(double numero) {
@@ -50,13 +48,13 @@ public class RestaGit {
 		}
 	
 	/** 
-	 * Este metodo resta el valor inicial de la variable "acumulado" (2000). Se ingresaria un numero real, cada vez que se ejecute el metodo
-	 * se restaria el numero ingresado al valor de la variable. Este metodo permite que el valor acumulado se guarde como una propiedad 
+	 * Este metodo resta el valor inicial de la variable "acumulado" (2000). Se ingresara un numero real, cada vez que se ejecute el metodo
+	 * se restara el numero ingresado al valor de la variable. Este metodo permite que el valor acumulado se guarde como una propiedad 
 	 * durante toda la clase. 
 	 * 
-	 * @param un solo numero real que se utilizaria para restar a la variable.
+	 * @param un solo numero real que se utilizara para restar a la variable.
 	 * 
-	 * @return devolveria el resultado de la resta cada vez que se utilice el metodo guardandose el valor tras cada resta realizada. 
+	 * @return devolvera el resultado de la resta cada vez que se utilice el metodo guardandose el valor tras cada resta realizada. 
 	 */
 
 	 // Este comentario es para probar Git
@@ -69,6 +67,6 @@ public class RestaGit {
 		return acumulado;
 
 	    } 
-// comentario para ver si me aparece en el status
+
 }
 
